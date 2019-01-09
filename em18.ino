5af46fe5500543391117c8f5d74c36b6b2e80cfe
fakade2 <46537030+fakade2@users.noreply.github.com>
2019-01-10 00:00:03 +0530

    Update em18.ino

--- em18.ino
+++ em18.ino
@@ -1,17 +1,26 @@
-char input[15]={};
-void setup()
-{ 
-  Serial.begin(9600);
-}
-void loop()
-{
-  if(Serial.available())
-  {
-    for(int i=0;i<15;i++)
-      input[i]=Serial.read();
+int count=0;
+char input[12];
+void setup() {
+ Serial.begin(9600);
 
-    for(int i=0;i<15;i++)
-      Serial.print(input[i]);
-    Serial.println();
+}
+
+void loop() {
+ if(Serial.available())
+ {
+  count=0;
+  while(Serial.available()&&count<12)
+  {
+    input[count]=Serial.read();
+    count++;
+    delay(5);
   }
+  Serial.print(input);
+  if((input[0]^input[2]^input[4]^input[6]^input[8]==input[10])&&(input[1]^input[3]^input[5]^input[7]^input[9]==input[11])){
+    Serial.println("No Error");
+  }
+  else
+  Serial.println("Error");
+ }
+
 }
